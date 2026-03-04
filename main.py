4a6cf241019b725102686d5eb95da30f150c6f34
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-04 18:52:39 +0800

    UPDATE:importing new file

--- main.py
+++ main.py
@@ -2,3 +2,24 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import Qt
 from app.components.tab import TopControlTab
+
+class NovaGazeOverlay(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowFlags(
+            Qt.FramelessWindowHint | 
+            Qt.WindowStaysOnTopHint | 
+            Qt.Tool
+        )
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        # Set window to full screen
+        screen_geo = QApplication.primaryScreen().geometry()
+        self.setGeometry(screen_geo)
+
+        # Initialize and position the Tab in the Top-Middle
+        self.top_tab = TopControlTab(self)
+        tab_x = (screen_geo.width() // 2) - (self.top_tab.width() // 2)
+        self.top_tab.move(tab_x, 0)
+        
