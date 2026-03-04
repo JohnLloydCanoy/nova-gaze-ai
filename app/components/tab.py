f05d1f2e804a8f0d0abb40be3df501469f6fff1d
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-04 18:48:37 +0800

    UPDATE: Setup the Look: 50% opacity and styling

--- app/components/tab.py
+++ app/components/tab.py
@@ -6,3 +6,8 @@ class TopControlTab(QWidget):
         super().__init__(parent)
         
         self.setFixedSize(200, 40)
+        self.setStyleSheet("""
+            background-color: rgba(40, 40, 40, 128); /* 50% Opacity Black */
+            border-bottom-left-radius: 10px;
+            border-bottom-right-radius: 10px;
+        """)
