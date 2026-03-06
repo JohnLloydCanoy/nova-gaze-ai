c99410776e1baaf0ade442dc7ff703e5fc962f18
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-06 15:59:49 +0800

    UPDATE: Integration testing file

--- test-nova.py
+++ test-nova.py
@@ -4,3 +4,5 @@ from app.aws_nova.client import NovaAIClient
 def test_api():
     print("🤖 Initializing Nova AI Client...")
     nova_client = NovaAIClient()
+    
+    test_image_path = "test_image.png"
