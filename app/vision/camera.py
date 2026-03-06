408742359c7404f047cee768fde8fa6f3428a052
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-06 08:48:47 +0800

    importing required dependencies

---
+++ app/vision/camera.py
@@ -0,0 +1,5 @@
+import cv2
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QGraphicsOpacityEffect
+from PySide6.QtCore import Qt, QThread, Signal, Slot
+from PySide6.QtGui import QImage, QPixmap
+
