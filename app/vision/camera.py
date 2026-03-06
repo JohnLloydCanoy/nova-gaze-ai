aebd73adb5ac8c89c47c13e4001856ee1b33bcc6
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-06 08:49:52 +0800

    adding

--- app/vision/camera.py
+++ app/vision/camera.py
@@ -3,3 +3,5 @@ from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QGraphicsOpacityEffe
 from PySide6.QtCore import Qt, QThread, Signal, Slot
 from PySide6.QtGui import QImage, QPixmap
 
+class CameraThread(QThread):
+    change_pixmap_signal = Signal(QImage)
