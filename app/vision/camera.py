70198a9ec9ebc87eb7c56f80d2e7c98f1dbbbcf2
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-06 08:53:23 +0800

    update

--- app/vision/camera.py
+++ app/vision/camera.py
@@ -13,3 +13,7 @@ class CameraThread(QThread):
         while self._run_flag:
             ret, cv_img = cap.read()
             if ret:
+                rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgb_image.shape
+                bytes_per_line = ch * w
+                qt_image = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
