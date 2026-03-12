7c392f92d43677cb72d19d65443d86667b2bcb02
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-12 10:08:49 +0800

    creating the function

--- app/logic/process/analysis.py
+++ app/logic/process/analysis.py
@@ -4,3 +4,5 @@ from app.aws_nova.client import NovaAIClient
 
 logger = logging.getLogger(__name__)
 
+def get_possible_ui_interactions(nova_client: NovaAIClient, image_path: str) -> list[dict]:
+    
