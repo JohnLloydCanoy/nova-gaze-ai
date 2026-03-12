d4f291216b88e7944deb042a3e60ec3d4791d1fc
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-12 15:16:06 +0800

    adding import

--- app/logic/procedure.py
+++ app/logic/procedure.py
@@ -1,2 +1,5 @@
 # This is were we sort out the procedure for the app, such as how to handle the data, how to train the model, and how to make predictions.
 
+import os 
+import logging
+from typing import List, Dict
