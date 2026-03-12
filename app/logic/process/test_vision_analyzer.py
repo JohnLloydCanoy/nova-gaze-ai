7f43d61210af46c925e93b4cc36f9c08feb5caee
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-12 14:17:30 +0800

    Create a fake Nova client so we don't make real web requests

--- app/logic/process/test_vision_analyzer.py
+++ app/logic/process/test_vision_analyzer.py
@@ -3,3 +3,7 @@ from unittest.mock import MagicMock
 from analysis import get_possible_ui_interactions
 
 class TestVisionAnalyzer(unittest.TestCase):
+    
+    def setUp(self):
+        self.mock_client = MagicMock()
+        self.test_image_path = "test_image.png"
