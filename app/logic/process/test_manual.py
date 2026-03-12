a24750833ee087a97a2f0314cb3a9df0c98bb96c
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-12 11:02:13 +0800

    created a function

--- app/logic/process/test_manual.py
+++ app/logic/process/test_manual.py
@@ -6,4 +6,4 @@ from vision_analyzer import get_possible_ui_interactions
 logging.basicConfig(level=logging.INFO)
 
 def run_manual_test():
-    
+    test_image_path = "sample_screen.png" ## Replace with your actual test image path
