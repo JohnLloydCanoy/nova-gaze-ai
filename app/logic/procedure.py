53ad9b6104455fe651686b8595384795464de520
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-12 15:16:49 +0800

    adding

--- app/logic/procedure.py
+++ app/logic/procedure.py
@@ -3,3 +3,6 @@
 import os 
 import logging
 from typing import List, Dict
+from app.aws_nova.client import NovaAIClient
+from app.logic.process.take_screen_shot import capture_screen 
+from app.logic.process.vision_analyzer import get_possible_ui_interactions
