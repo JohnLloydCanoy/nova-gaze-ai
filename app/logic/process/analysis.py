aa3c3e65c8a15b679d79e78f51ba851982c49cb6
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-12 10:09:23 +0800

    adding the note

--- app/logic/process/analysis.py
+++ app/logic/process/analysis.py
@@ -5,4 +5,12 @@ from app.aws_nova.client import NovaAIClient
 logger = logging.getLogger(__name__)
 
 def get_possible_ui_interactions(nova_client: NovaAIClient, image_path: str) -> list[dict]:
-    
+    """_summary_
+
+    Args:
+        nova_client (NovaAIClient): _description_
+        image_path (str): _description_
+
+    Returns:
+        list[dict]: _description_
+    """
