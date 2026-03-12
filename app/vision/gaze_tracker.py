08bdf96a79e4ad115ebaea7715bf3f7aa4d48c5d
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-12 20:10:59 +0800

    Initialize MediaPipe Face Mesh

--- app/vision/gaze_tracker.py
+++ app/vision/gaze_tracker.py
@@ -6,3 +6,10 @@ import numpy as np
 
 class GazeAnalyzer:
     def __init__(self):
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = self.mp_face_mesh.FaceMesh(
+            max_num_faces=1,
+            refine_landmarks=True, 
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
